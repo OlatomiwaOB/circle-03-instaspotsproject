8f986d89d248e7d568f477d66bcdb15e07c7cd79
Iranloye Hannah <iranloye8.hannah@gmail.com>
2025-05-20 18:24:15 +0100

    Card Modal Functionality Added

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const cards = [
-    {
-        "image": "assets/images/pexels-kassandre-pedro-8639743 1-6.png",
-        "text": "Val Thorens",
-        "name": "image of a Val Thorens"
-    },
-    {
-        "image": "assets/images/pexels-kassandre-pedro-8639743 1-1.png",
-        "text": "Restaurant terrace",
-        "name": "image of a Restaurant"
-    },
-    {
-        "image": "assets/images/pexels-kassandre-pedro-8639743 1-2.png",
-        "text": "An outdoor cafe",
-        "name": "love icon"
-    },
-    {
-        "image": "assets/images/pexels-kassandre-pedro-8639743 1-3.png",
-        "text": "A very long bridge, over the forest...",
-        "name": "image of a bridge"
-    },
-    {
-        "image": "assets/images/pexels-kassandre-pedro-8639743 1-4.png",
-        "text": "Tunnel with morning light",
-        "name": "image of a Tunnel"
-    },
-    {
-        "image": "assets/images/pexels-kassandre-pedro-8639743 1-5.png",
-        "text": "Mountain house",
-        "name": "image of a Mountain"
-    }
-]
+  {
+    image: "assets/images/pexels-kassandre-pedro-8639743 1-6.png",
+    text: "Val Thorens",
+    name: "image of a Val Thorens",
+  },
+  {
+    image: "assets/images/pexels-kassandre-pedro-8639743 1-1.png",
+    text: "Restaurant terrace",
+    name: "image of a Restaurant",
+  },
+  {
+    image: "assets/images/pexels-kassandre-pedro-8639743 1-2.png",
+    text: "An outdoor cafe",
+    name: "love icon",
+  },
+  {
+    image: "assets/images/pexels-kassandre-pedro-8639743 1-3.png",
+    text: "A very long bridge, over the forest...",
+    name: "image of a bridge",
+  },
+  {
+    image: "assets/images/pexels-kassandre-pedro-8639743 1-4.png",
+    text: "Tunnel with morning light",
+    name: "image of a Tunnel",
+  },
+  {
+    image: "assets/images/pexels-kassandre-pedro-8639743 1-5.png",
+    text: "Mountain house",
+    name: "image of a Mountain",
+  },
+];
 
 // Get DOM elements
 const editBtn = document.querySelector("#edit_btn");
@@ -40,7 +40,7 @@ const profileForm = document.getElementById("profileForm");
 const profileName = document.getElementById("profileName");
 const profileBio = document.getElementById("profileBio");
 const profileImage = document.getElementById("profileImage");
-const cardsContainer = document.querySelector('.cards')
+const cardsContainer = document.querySelector(".cards");
 
 // Get current profile data
 const currentName = document.querySelector(".text1 h1").textContent;
@@ -90,24 +90,63 @@ profileForm.addEventListener("submit", function (e) {
 
   closeModal();
 });
-
+// DisplayCard function itierating over each card data
 function displayCard(cards) {
-    cardsContainer.innerHTML = ``;
-    if (cards.length > 0) {
-        cards.forEach(card => {
-            const cardsItem = document.createElement('div');
-            cardsItem.classList.add('card');
-            cardsItem.innerHTML = `
+  cardsContainer.innerHTML = ``;
+  if (cards.length > 0) {
+    cards.forEach((card) => {
+      const cardsItem = document.createElement("div");
+      cardsItem.classList.add("card");
+      cardsItem.innerHTML = `
                 <img src="${card.image} "alt="${card.name}"/>
                 <div class="card-text">
                     <p>${card.text}</p>
                     <img src="./assets/images/Union.svg" alt="love icon" />
                 </div>
             `;
-            cardsContainer.append(cardsItem)
-            console.log('worked')
-        })
-    }
+      cardsContainer.append(cardsItem);
+    });
+  }
 }
+displayCard(cards);
 
-displayCard(cards)
+//Cards Modal
+const cardElements = document.querySelectorAll(".card");
+
+// Get card Modal Element
+const modalImg = document.getElementById("modalImg");
+const modalText = document.querySelector(".modalText p");
+const modalTextImg = document.querySelector(".modalText img");
+const imageModal = document.getElementById("imageModal");
+const closeImgBtn = document.querySelector(".imgModal .img-content .close");
+
+//mapping through cards
+cardElements.forEach((card) => {
+  // Getting each card properties
+  const cardImg = card.querySelector("img");
+  const cardText = card.querySelector(".card-text p").textContent;
+  const cardTextImg = card.querySelector(".card-text img");
+
+  card.addEventListener("click", () => {
+    imageModal.style.display = "block";
+    //checking
+    if (cardImg && cardText) {
+      modalImg.src = cardImg.src;
+      modalText.textContent = cardText;
+      modalTextImg.src = cardTextImg.src;
+      modalTextImg.alt = cardTextImg.alt;
+    }
+  });
+});
+
+// Close cardModal
+function closeCardModal() {
+  imageModal.style.display = "none";
+}
+closeImgBtn.addEventListener("click", closeCardModal);
+// Close cardModal when clicking outside
+window.addEventListener("click", (e) => {
+  if (e.target === imageModal) {
+    closeCardModal();
+  }
+});
